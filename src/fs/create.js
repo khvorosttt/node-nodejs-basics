33fcdefff0f5690612d36c60dbfd4804ec9b3c7d
khvorosttt <khvorosttt@yandex.by>
2025-10-24 00:47:33 +0300

    feat: add realization create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { join } from 'path';
+import { open, access } from 'node:fs/promises';
+
 const create = async () => {
-  // Write your code here
+  const dirname = import.meta.dirname;
+  const filePath = join(dirname,'files', 'fresh.txt');
+  const msg = 'I am fresh and young';
+  const errMsg = 'FS operation failed';
+  try{
+    await access(filePath);
+    throw new Error();
+  } catch(err) {
+    if (err.code !== 'ENOENT') throw new Error(errMsg);
+  }
+  const file = await open(filePath, 'w');
+  await file.write(msg);
+  await file?.close();
+   
 };
 
 await create();

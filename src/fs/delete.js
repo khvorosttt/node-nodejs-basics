3dfbae24029e01d82adf16bf01b3b4ac5b5d0597
khvorosttt <khvorosttt@yandex.by>
2025-10-24 01:19:57 +0300

    feat: add realization delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { join } from 'path';
+import { rm } from 'node:fs/promises';
 const remove = async () => {
-  // Write your code here
+  const dirname = import.meta.dirname;
+  const deletePath = join(dirname,'files', 'fileToRemove.txt');
+  const errMsg = 'FS operation failed';
+  try{
+    await rm(deletePath);
+  } catch(err) {
+    throw new Error(errMsg);
+  }
 };
 
 await remove();

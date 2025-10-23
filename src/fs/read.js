711a21c56912957f8b6b7cf58b209cd6e6c64144
khvorosttt <khvorosttt@yandex.by>
2025-10-24 01:37:13 +0300

    feat: add realization read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { join } from 'path';
+import {readFile, access} from 'node:fs/promises';
 const read = async () => {
-  // Write your code here
+  const dirname = import.meta.dirname;
+  const filePath = join(dirname,'files', 'fileToRead.txt');
+  const errMsg = 'FS operation failed'
+  try{
+    await access(filePath);
+  } catch(err) {
+    throw new Error(errMsg);
+  }
+  const content = await readFile(filePath, { encoding: 'utf8' });
+  console.log(content);
 };
 
 await read();

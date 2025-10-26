7a90567d72ec7131ec84193d931d1a453d70c5a6
khvorosttt <khvorosttt@yandex.by>
2025-10-27 00:59:00 +0300

    feat: add realization wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from 'os';
+import { join } from 'path';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-  // Write your code here
+  const CPUCoresLength = os.cpus().length;
+  const workers = [];
+  const dirname = import.meta.dirname;
+  const workerPath = join(dirname, 'worker.js')
+  for(let i=0; i<CPUCoresLength;i++){
+    workers.push(new Worker(workerPath, {
+      workerData: 10 + i
+    }));
+  }
+  const result = await Promise.all(
+    workers.map((worker) => new Promise((resolve) => {
+      worker.on('message', (msg) => {
+        resolve({ status: 'resolved', data: msg });
+      });
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+      });
+    }))
+  );
+  console.log(result);
 };
 
 await performCalculations();

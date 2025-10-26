4e66243524720fc5041939f43da0978fb16296ab
khvorosttt <khvorosttt@yandex.by>
2025-10-26 21:01:47 +0300

    feat: add realization streams/write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const write = async () => {
-  // Write your code here
+  const filePath=join(import.meta.dirname, 'files', 'fileToWrite.txt');
+  console.log('Press Ctrl+C to end');
+  await pipeline(process.stdin, createWriteStream(filePath));
 };
 
 await write();

c621fc637a166247435690494836637eb5ed1ff2
khvorosttt <khvorosttt@yandex.by>
2025-10-27 02:16:59 +0300

    feat: add realization cp functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { join } from 'path';
+import { fork } from 'child_process';
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const dirname = import.meta.dirname;
+  const filePath = join(dirname, 'files', 'script.js');
+  const cp = fork(filePath, args, { silent: true });
+  process.stdin.pipe(cp.stdin);
+  cp.stdout.pipe(process.stdout);
+  cp.stderr.pipe(process.stderr);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+//spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1, 2, 3]);

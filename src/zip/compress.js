17f913ec74261375ce927052903a2938eefb3e82
khvorosttt <khvorosttt@yandex.by>
2025-10-26 21:51:13 +0300

    feat: add realization compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-  // Write your code here
+  const dirname = import.meta.dirname;
+  const fileFromPath=join(dirname, 'files', 'fileToCompress.txt');
+  const fileToPath=join(dirname, 'files', 'archive.gz');
+  const rs = createReadStream(fileFromPath);
+  const ws = createWriteStream(fileToPath);
+  await pipeline(rs, createGzip(), ws);
 };
 
 await compress();
